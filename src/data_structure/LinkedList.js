aa1ab57bdf3fea834c1b2053aac8e50a530bda2b
Hyun Seung Jeong <ahribori@gmail.com>
2017-05-19 16:05:10 +0900

    이중 연결 리스트 작성

--- src/data_structure/LinkedList.js
+++ src/data_structure/LinkedList.js
@@ -1,49 +1,11 @@
 /**
- * 연결리스트(ArrayList)에서 사용할 Node 자료형
+ * 연결리스트(LinkedList)에서 사용할 노드 클래스
  */
 class Node {
-    constructor(_prev, _item, _next) {
-        /**
-         * Private values
-         */
-        let prev = _prev;
-        let item = _item;
-        let next = _next;
-
-        /**
-         * Getter methods
-         */
-        this.getPrev = () => prev;
-        this.getItem = () => item;
-        this.getNext = () => next;
-
-        /**
-         * Setter methods
-         */
-        this.setPrev = _prev => {
-            prev = _prev;
-            return this;
-        };
-        this.setItem = _item => {
-            item = _item;
-            return this;
-        };
-        this.setNext = _next => {
-            next = _next;
-            return this;
-        };
-
-        /**
-         * print this object
-         */
-        this.print = () => {
-            console.log({
-                prev,
-                item,
-                next
-            });
-        }
-
+    constructor(_prev, _data, _next) {
+        this.prev = _prev;
+        this.data = _data;
+        this.next = _next;
     }
 }
 
@@ -52,18 +14,165 @@ class Node {
  */
 export default class LinkedList {
     constructor() {
-        this.data = [];
-    }
+        let head = null;
+        let tail = null;
+        let size = 0;
 
-    add(index, item) {
+        /**
+         * 해당 인덱스의 노드를 반환
+         */
+        const node = (index) => {
+            if (index < size / 2 ) {
+                // 인덱스가 사이즈의 절반보다 작으면 앞(head)에서부터부터 탐색
+                let cursor = head;
+                for (let i = 0; i < index; i++) {
+                    cursor = cursor.next;
+                }
+                return cursor;
+            } else {
+                // 인덱스가 사이즈의 절반보다 크면 뒤(tail)에서부터 탐색
+                let cursor = tail;
+                for (let i = size -1; i > index; i--) {
+                    cursor = cursor.prev;
+                }
+                return cursor;
+            }
+        };
 
-    }
+        /**
+         * 리스트의 맨 앞에 노드를 추가
+         * @param data
+         */
+        this.addFirst = (data) => {
+            const newNode = new Node(null, data, head);
+            if (head !== null) {
+                head.prev = newNode;
+            }
+            head = newNode;
+            size ++;
+            if (head.next === null) {
+                tail = newNode;
+            }
+        };
 
-    remove() {
+        /**
+         * 리스트의 맨 뒤에 노드를 추가
+         * @param data
+         */
+        this.addLast = (data) => {
+            const newNode = new Node(tail, data, null);
+            if (tail !== null) {
+                tail.next = newNode;
+            }
+            tail = newNode;
+            size ++;
+            if (tail.prev === null) {
+                head = newNode;
+            }
+        };
 
-    }
+        /**
+         * 인덱스를 지정해 노드를 추가
+         * @param k
+         * @param data
+         */
+        this.add = (k, data) => {
+            const prev = node(k-1);
+            if (prev === null) {
+                this.addFirst(data);
+            } else {
+                const next = prev.next;
+                const newNode = new Node(prev, data, next);
+                prev.next = newNode;
+                if (next !== null) {
+                    next.prev = newNode;
+                }
+                size ++;
+                if (next === null) {
+                    tail = newNode;
+                }
+            }
+        };
 
-    size () {
+        /**
+         * 리스트의 맨 앞 노드를 삭제
+         */
+        this.removeFirst = () => {
+            if (head) {
+                let temp = head;
+                head = temp.next;
+                if (head === null) {
+                    tail = null;
+                } else {
+                    head.prev = null;
+                }
+                temp = null;
+                size--;
+            }
+        };
+
+        /**
+         * 리스트의 맨 뒤 노드를 삭제
+         */
+        this.removeLast = () => {
+            if (tail) {
+                let temp = tail;
+                tail = temp.prev;
+                if (tail === null) {
+                    head = null;
+                } else {
+                    tail.next = null;
+                }
+                temp = null;
+                size --;
+            }
+        };
+
+        /**
+         * 인덱스를 지정해 노드를 삭제
+         * @param index
+         */
+        this.remove = (index) => {
+            let target = node(index);
+            if (target) {
+                const prev = target.prev;
+                const next = target.next;
+
+                if (prev === null) {
+                    return this.removeFirst();
+                }
+
+                if (next === null) {
+                    return this.removeLast();
+                }
+
+                prev.next = target.next;
+                next.prev = target.prev;
+                target = null;
+                size --;
+            }
+        };
+
+        /**
+         * Head를 반환
+         */
+        this.getHead = () => head;
+
+        /**
+         * Tail을 반환
+         */
+        this.getTail = () => tail;
+
+        /**
+         * 해당 인덱스의 노드를 반환
+         * @param k
+         */
+        this.get = (k) => node(k);
+
+        /**
+         * 리스트의 크기를 반환
+         */
+        this.size = () => size;
 
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@ import LinkedList from './data_structure/LinkedList';
 
 const linkedList = new LinkedList();
 
-console.log(linkedList);
+//-----삽입-----
+linkedList.addFirst('사과'); // 사과
+linkedList.addFirst('귤'); // 귤-사과
+linkedList.addLast('바나나'); // 귤-사과-바나나
+linkedList.addLast('오렌지'); // 귤-사과-바나나-오렌지
+linkedList.add(2, '레몬'); // 귤-사과-레몬-바나나-오렌지
+
+//-----삭제-----
+linkedList.removeFirst(); // 사과-레몬-바나나-오렌지
+linkedList.removeLast(); // 사과-레몬-바나나
+linkedList.remove(3);
+
+const result = [];
+let cursor = linkedList.getHead();
+
+while(cursor) {
+    result.push(cursor.data);
+    cursor = cursor.next;
+}
+
+console.log(result);
